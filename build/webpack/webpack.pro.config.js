45bcc5b820f61293ce18c8d9e3a259e0f9101f26
孟刘 <171626037@qq.com>
2022-04-05 21:19:07 +0800

    生产环境配置

--- build/webpack/webpack.pro.config.js
+++ build/webpack/webpack.pro.config.js
@@ -3,7 +3,6 @@ const { merge } = require('webpack-merge')
 const BaseConfig = require('./webpack.base.config')
 const config = require('../../config')
 
-
 module.exports = merge(BaseConfig, {
     mode: "production",
 })
